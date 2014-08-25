473df9deb7b4da0cfd537ef29d835c2ad60e86d5
danielgmerchanb <daniel.merchan2@carvajal.com>
2014-08-25 13:59:30 -0500

    Proyecto GetBulkHostAddresses

---
+++ Visual Studio 2013/ChangePasswordClientWeb/ChangePasswordClientWeb/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangePasswordClientWeb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var wb = new WebClient())
+            {
+                string url = "http://54.84.219.185/~carvajal/ChangePassword/controllers/WsController.php";
+                var data = new NameValueCollection();
+
+                data["domain"] = "aisfood.com";
+                data["password"] = "aisfood#.$%203427166";
+
+                var response = wb.UploadValues(url, "POST", data);
+
+                var responseString = Encoding.UTF8.GetString(response);
+            }
+        }
+    }
+}
